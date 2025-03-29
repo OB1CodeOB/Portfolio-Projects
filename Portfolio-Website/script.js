b11929bc8f347034909ad2154b6aeeeb1df53044
OB1CodeOB <102632526+OB1CodeOB@users.noreply.github.com>
2025-03-29 09:49:25 -0700

    update

--- Portfolio-Website/script.js
+++ Portfolio-Website/script.js
@@ -1,4 +1,8 @@
 // Get elements from DOM
-
+const lightsaberHilt = document.getElementById('lightsaber-hilt');
+const lightsaberBlade = document.getElementById('lightsaber-blade');
 
 // Event listeners
+lightsaberHilt.addEventListener('click', () => {
+    lightsaberBlade.classList.toggle('blade-extend');
+})
