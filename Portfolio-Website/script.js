0899daf34446966c97cef2be86ea6265ff1c9870
OB1CodeOB <102632526+OB1CodeOB@users.noreply.github.com>
2025-03-30 14:12:53 -0700

    update

---
+++ Portfolio-Website/script.js
@@ -0,0 +1,10 @@
+// OPEN BLASTDOOR
+
+const topBlastdoor = document.getElementById('top-blastdoor');
+const bottomBlastdoor = document.getElementById('bottom-blastdoor');
+
+topBlastdoor.addEventListener('click', () => {
+    topBlastdoor.style.height = "15vh";
+    bottomBlastdoor.style.height = "15vh";
+})
+
