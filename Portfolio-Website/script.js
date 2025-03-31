04018ef55f9f3d9f4419031b21e4a116928af699
OB1CodeOB <102632526+OB1CodeOB@users.noreply.github.com>
2025-03-31 11:02:25 -0700

    update

--- Portfolio-Website/script.js
+++ Portfolio-Website/script.js
@@ -1,17 +1,36 @@
-// OPEN BLASTDOOR
+// HIDE PROFILE AVATAR AND OPEN BLASTDOOR
 
+const profileAvatar = document.getElementById('profile-avatar');
 const topBlastdoor = document.getElementById('top-blastdoor');
 const bottomBlastdoor = document.getElementById('bottom-blastdoor');
 
-topBlastdoor.addEventListener('click', () => {
+// Hide avatar
+function avatarHide() {
+    profileAvatar.style.right = "-300px";
+}
+
+// Open blastdoor
+function blastdoorOpen() {
     topBlastdoor.style.height = "6vh";
     bottomBlastdoor.style.height = "6vh";
+}
+
+// When blast door is clicked, hide avatar, wait for a sec, then open blastdoor
+topBlastdoor.addEventListener('click', () => {
+    avatarHide();
+    setTimeout(() => {
+        blastdoorOpen();
+    }, 1000);   
 })
 
 bottomBlastdoor.addEventListener('click', () => {
-    topBlastdoor.style.height = "6vh";
-    bottomBlastdoor.style.height = "6vh";
+    avatarHide();
+    setTimeout(() => {
+        blastdoorOpen();
+    }, 1000);   
 })
+
+
 
 
 
