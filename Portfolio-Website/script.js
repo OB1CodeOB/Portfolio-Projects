1b8bff0ee72df802c1afc90bb28b063f1beee874
OB1CodeOB <102632526+OB1CodeOB@users.noreply.github.com>
2025-03-31 08:14:43 -0700

    update

--- Portfolio-Website/script.js
+++ Portfolio-Website/script.js
@@ -4,7 +4,14 @@ const topBlastdoor = document.getElementById('top-blastdoor');
 const bottomBlastdoor = document.getElementById('bottom-blastdoor');
 
 topBlastdoor.addEventListener('click', () => {
-    topBlastdoor.style.height = "15vh";
-    bottomBlastdoor.style.height = "15vh";
+    topBlastdoor.style.height = "6vh";
+    bottomBlastdoor.style.height = "6vh";
 })
+
+bottomBlastdoor.addEventListener('click', () => {
+    topBlastdoor.style.height = "6vh";
+    bottomBlastdoor.style.height = "6vh";
+})
+
+
 
